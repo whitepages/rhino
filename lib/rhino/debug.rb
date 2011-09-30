1863bd491a0477d53b1d36391349e0e93c737498
Jay Hoover <jhoover@whitepages.com>
2011-09-30 12:05:46 -0700

    make debug be a settable flag instead of a const

--- lib/rhino/debug.rb
+++ lib/rhino/debug.rb
@@ -1,11 +1,17 @@
 module Rhino
   module Debug
+    @@enable_debug = false
+    
+    def self.enable_debug
+      @@enable_debug = true
+    end
+    
     def debug(str)
-      puts "\e[33mDEBUG: #{str}\e[0m" if RHINO_DEBUG
+      puts "\e[33mDEBUG: #{str}\e[0m" if @@enable_debug
     end
 
     def highlight(str)
-      puts "\e[35m**** #{str}\e[0m" if RHINO_DEBUG
+      puts "\e[35m**** #{str}\e[0m" if @@enable_debug
     end
 
     def hie(obj)
